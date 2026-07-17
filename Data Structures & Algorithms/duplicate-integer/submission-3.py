6321a28fa26e6aa35a6a8c5822bf79155a96b6fc
abhitiw369-afk <abhitiw369@gmail.com>
2026-07-17 22:16:22 +0530

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def hasDuplicate(self, nums : list[int]) -> bool:
+
+        hashset = set()
+
+        for i in nums:
+
+            if i in hashset :
+
+                return True
+            hashset.add(i)
+
+        return False
+            
+            
+
+        
+
+
+
