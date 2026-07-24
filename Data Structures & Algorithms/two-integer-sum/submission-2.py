e74692acfa54aef7d5325562553b6102b1a51884
abhitiw369-afk <abhitiw369@gmail.com>
2026-07-24 08:54:29 +0530

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        # prevMap = {} #empty hashmap to check in O(1)
+        
+        # for i , n in enumerate(nums):
+        #     diff = target - n
+        #     if diff in prevMap:
+        #         return [prevMap[diff], i] #it gives value(index) of diff(key)
+            
+        #     prevMap[n] = i #index is working as a value
+
+
+        hmap = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in hmap:
+                return [hmap[diff],i]
+            hmap[n] = i
