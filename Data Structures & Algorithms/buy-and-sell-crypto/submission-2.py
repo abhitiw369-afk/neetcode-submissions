34f8270847e34078e3ab101c7e2ea9dddeb3a0d9
abhitiw369-afk <abhitiw369@gmail.com>
2026-07-30 23:22:42 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profitmax = 0
+        l = 0
+        for r in range(len(prices)) :
+            if prices[l] >= prices[r] : #loss days or 0 profit days
+                l = r #bcz if cp > sp, make cp as sp, as sp is lower 
+            else : #profit day
+                profit = prices[r] - prices[l]
+                profitmax = max(profitmax,profit)
+        return profitmax
