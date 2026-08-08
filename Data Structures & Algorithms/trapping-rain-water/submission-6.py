f5e6b015381df1ba0c519696be76cfd3b98ff25d
Abhishek Tiwari <abhitiw369@gmail.com>
2026-08-09 00:38:30 +0530

    Add: trapping-rain-water - submission-6

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-6.py
@@ -0,0 +1,118 @@
+# class Solution:
+#     def trap(self, height: List[int]) -> int:
+        # area = 0
+        # l, r =0, len(height)-1
+        # leftMax = height[l]
+        # rightMax = height[r]
+
+        # while l < r:
+        #     if leftMax < rightMax:
+        #         l += 1
+        #         leftMax = max(leftMax, height[l])
+        #         area += leftMax - height[l]
+        #     else:
+        #         r -= 1
+        #         rightMax = max(rightMax, height[r])
+        #         area += rightMax - height[r]    
+        # return area
+
+# class Solution:
+#     def trap(self, height: List[int]) -> int:
+
+        # maxarea = 0
+        # l = 0
+        # r = len(height)-1
+        # maxL = height[l]
+        # maxR = height[r]
+        # while l < r :
+        #     if maxL < maxR :
+        #         l+=1
+        #         maxL = max(maxL,height[l])
+        #         maxarea += maxL-height[l]
+        #     else :
+        #         r-=1
+        #         maxR=max(maxR,height[r])
+        #         maxarea += maxR-height[r]
+
+        # return maxarea
+
+        # totalarea = 0
+        # l = 0
+        # r = len(height)-1
+        # maxL = height[l]
+        # maxR = height[r]
+        # while l < r :
+        #     if maxL < maxR :
+        #         l += 1
+        #         maxL = max(maxL,height[l]) 
+        #         totalarea += maxL - height[l]
+                
+        #     else :
+        #         r -= 1
+        #         maxR = max(maxR,height[r]) 
+        #         totalarea += maxR - height[r]
+                
+        # return totalarea
+
+
+
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        totalarea = 0
+        l = 0
+        maxl=height[l]
+        r = len(height)-1
+        maxr = height[r]
+        while l < r :
+
+            if maxl < maxr :
+                l += 1
+                maxl = max(maxl,height[l])
+                
+                totalarea += maxl - height[l] 
+            
+            else :
+                r -= 1
+                maxr = max(maxr, height[r])
+                
+                totalarea += maxr - height[r]
+
+        return totalarea
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
