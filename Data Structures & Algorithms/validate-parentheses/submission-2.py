519b57881cb91813e01404c785137c94b25ac8bb
Abhishek Tiwari <abhitiw369@gmail.com>
2026-08-11 21:39:01 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        seen = {'}':'{',')':'(',']':'['} # mapped every closing(key) with opening(value)
+        stack = [] #stack as a dynamic array
+        for c in s:
+
+            if c in seen :
+                if stack and stack[-1] == seen[c] :
+                    stack.pop()
+                else :
+                    return False
+
+            else :
+                stack.append(c)
+        return True if not stack else False
+        
