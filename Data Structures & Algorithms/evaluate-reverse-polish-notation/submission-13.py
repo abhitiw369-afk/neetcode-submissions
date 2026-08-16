94f140198af252199a1c60c482114388e41982be
Abhishek Tiwari <abhitiw369@gmail.com>
2026-08-16 19:31:30 +0530

    Add: evaluate-reverse-polish-notation - submission-13

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-13.py
@@ -0,0 +1,63 @@
+# class Solution:
+#     def evalRPN(self, tokens: List[str]) -> int:
+        # stack = []
+        # for s in tokens :
+        #     if s=='+' :
+        #         stack.append(int(stack.pop()+stack.pop()))
+        #     elif s=='-' :
+        #         a,b = stack.pop(),stack.pop()
+        #         stack.append(int(b-a))
+        #     elif s=='*' :
+        #         stack.append(int(stack.pop()*stack.pop()))
+        #     elif s=='/' :
+        #         a,b = stack.pop(),stack.pop()
+        #         stack.append(int(b/a))
+            
+        #     else :
+        #         stack.append(int(s))
+        
+        # return stack[0]
+
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        
+        stack = []
+
+        for s in tokens :
+            if s == '+':
+                stack.append(stack.pop()+stack.pop())
+            elif s == '-':
+                a, b = stack.pop(),stack.pop()
+                stack.append(b-a)
+            elif s == '*':
+                stack.append(stack.pop()*stack.pop())
+            elif s == '/':
+                a,b = stack.pop(),stack.pop()
+                stack.append(int(b/a))
+            else :
+                stack.append(int(s))
+        return stack[-1]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
