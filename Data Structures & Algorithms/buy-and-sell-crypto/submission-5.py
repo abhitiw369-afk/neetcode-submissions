17cad4880b75225d9333111bf27c8c970adec328
Abhishek Tiwari <abhitiw369@gmail.com>
2026-08-26 19:27:02 +0530

    Add: buy-and-sell-crypto - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-5.py
@@ -0,0 +1,81 @@
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+        # profitmax = 0
+        # l = 0
+        # for r in range(len(prices)) :
+        #     if prices[l] >= prices[r] : #loss days or 0 profit days
+        #         l = r #bcz if cp > sp, make cp as sp, as sp is lower 
+        #     else : #profit day
+        #         profit = prices[r] - prices[l]
+        #         profitmax = max(profitmax,profit)
+        # return profitmax
+
+
+
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         profitmax = 0
+#         l = 0
+#         for r in range(len(prices)) :
+#             profit = prices[r] - prices[l]
+            
+#             if profit > 0 :
+#                 profitmax = max(profitmax, profit)
+                
+#             else:
+#                 l = r
+#         return profitmax
+
+
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        maxP=0
+        l=0
+        for r in range(len(prices)):
+                profit=prices[r]-prices[l]
+                if profit > 0:
+                        maxP=max(maxP,profit)
+                else:
+                        l=r
+        return maxP
+
+        
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
