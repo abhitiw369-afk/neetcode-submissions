8ade5c5fb28761c0c722325ed30a16ab5558276f
Abhishek Tiwari <abhitiw369@gmail.com>
2026-09-03 21:12:56 +0530

    Add: validate-parentheses - submission-9

---
+++ Data Structures & Algorithms/validate-parentheses/submission-9.py
@@ -0,0 +1,91 @@
+# class Solution:
+#     def isValid(self, s: str) -> bool:
+        
+#         seen = {'}':'{',')':'(',']':'['} # mapped every closing(key) with opening(value)
+#         stack = [] #stack as a dynamic array
+#         for c in s:
+
+#             if c in seen :
+#                 if stack and stack[-1] == seen[c] :
+#                     stack.pop()
+#                 else :
+#                     return False
+
+#             else :
+#                 stack.append(c)
+#         return True if not stack else False
+        
+
+# class Solution:
+#     def isValid(self, s: str) -> bool:
+
+        # seen = {'}':'{',']':'[',')':'('}
+
+        # stack = []
+
+        # for c in s :
+        #     if c in seen :
+        #         if stack and stack[-1]==seen[c]:
+        #             stack.pop()
+
+        #         else :
+        #             return False
+                    
+        #     else :
+        #         stack.append(c)
+        # return True if not stack else False #if stack at end becomes empty
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        seen = {'}':'{',']':'[',')':'('}
+        stack = []
+
+        for c in s :
+            if c in seen:
+                if stack and stack[-1]==seen[c] :
+                    stack.pop()
+                else :
+                    return False
+            else:
+                stack.append(c)
+        return True if not stack else False
+        
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        hmap={'}':'{',']':'[',')':'('}
+        stack=[]
+        for c in s:
+            if c in hmap:
+                if stack and stack[-1]==hmap[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        return True if not stack else False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
