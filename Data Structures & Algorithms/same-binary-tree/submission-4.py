6e612424f4b60e536176f2dd4d269722650b9993
Abhishek Tiwari <abhitiw369@gmail.com>
2026-09-22 20:27:15 +0530

    Add: same-binary-tree - submission-4

---
+++ Data Structures & Algorithms/same-binary-tree/submission-4.py
@@ -0,0 +1,61 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q: return True
+        if ((not p or not q) or(p.val!=q.val)): return False
+
+        return (self.isSameTree(p.left,q.left)and self.isSameTree(p.right,q.right))
+
+
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+
+        if not p and not q:
+            return True
+        
+        if ((not p or not q) and (p.val!=q.val)): return False
+
+        l=self.isSameTree(p.left,q.left)
+        r=self.isSameTree(p.right,q.right)
+
+        return (l and r and p.val==q.val)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q: return True
+        if ((not p or not q) or(p.val!=q.val)): return False
+        l=self.isSameTree(p.left,q.left)
+        r=self.isSameTree(p.right,q.right)
+        return (l and r)
+
