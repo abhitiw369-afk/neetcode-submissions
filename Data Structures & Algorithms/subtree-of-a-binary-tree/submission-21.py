7a55f8c97525649dfb4cc04439b9d3985a9ec6cb
Abhishek Tiwari <abhitiw369@gmail.com>
2026-09-23 10:33:37 +0530

    Add: subtree-of-a-binary-tree - submission-21

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-21.py
@@ -0,0 +1,66 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:   
+    def isSubtree(self, s: Optional[TreeNode], t: Optional[TreeNode]) -> bool:
+        if not s: return False
+        if s and not t: return True
+
+        if self.sameTree(s,t):
+            return True
+
+        return (self.isSubtree(s.left,t) or self.isSubtree(s.right,t))
+
+    def sameTree(self,s,t):
+        if not s and not t: return True
+        if ((not s or not t) or (s.val!=t.val)): return False
+        l=self.sameTree(s.left,t.left)
+        r=self.sameTree(s.right,t.right)
+        return (l and r)
+
+
+
+
+
+
+class Solution:   
+    def isSubtree(self, s: Optional[TreeNode], t: Optional[TreeNode]) -> bool:
+        
+
+        def sameTree(s,t): #while using inner helper fxn, we've to define it first before calling
+            if not s and not t: return True
+            if ((not s or not t) or s.val!=t.val): return False
+
+            l=sameTree(s.left,t.left)
+            r=sameTree(s.right,t.right)
+
+            return (l and r and s.val==t.val)
+        
+        #for isSubtree, now
+
+        if not s and not t: return True
+        if s and not t: return True
+        if not s and t: return False
+
+        if sameTree(s,t): return True
+
+        return(self.isSubtree(s.left,t) or
+        self.isSubtree(s.right,t))
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
